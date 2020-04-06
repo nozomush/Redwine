15b45863877b9f5bd0f5b168a5fceaaf8a8eb5d3
ArmanTursun <ArmanTursun@users.noreply.github.com>
2020-04-06 15:21:21 +0900

    gray
    gray

--- show_tom.py
+++ show_tom.py
@@ -11,7 +11,9 @@ path = 'image/tom.jpg'
 
 def main():
     img = cv2.imread(path)
-    cv2.imshow('tom',img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('tom',gray)
+
     
     cv2.waitKey(0)
     cv2.destroyAllWindows()
