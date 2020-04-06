4949c4e0597529e96b140b85f1bfeac9b3ed2b37
shiraiz <snozomus@gmail.com>
2020-04-06 12:20:05 +0900

    Add show_tom.py and image/tom.jpg

---
+++ show_tom.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 12:16:07 2020
+
+@author: snozo
+"""
+
+import cv2
+
+path = 'image/tom.jpg'
+
+img = cv2.imread(path)
+cv2.imshow('tom',img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
